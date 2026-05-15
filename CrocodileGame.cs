51102c8154e06b66ba374012f485512cbe94f183
Malin Fossum <malinfossum.dev@proton.me>
2026-05-15 16:42:11 +0200

    feat: implement CrocodileGame

--- CrocodileGame.cs
+++ CrocodileGame.cs
@@ -1,10 +1,40 @@
 namespace CsharpFundamentals;
 
-// Curriculum: Krokodillespillet
+// Modul 2: Krokodillespillet
 public class CrocodileGame
 {
     public void Run()
     {
-        Console.WriteLine("Not yet implemented");
+        var score = 0;
+        while (true)
+        {
+            var a = Random.Shared.Next(1, 12);
+            var b = Random.Shared.Next(1, 12);
+            Console.WriteLine($"{a} _ {b}");
+            Console.Write("Svar (<, >, =): ");
+            var input = Console.ReadLine();
+
+            if (input != "<" && input != ">" && input != "=")
+            {
+                Console.WriteLine($"Spillet er over. Sluttpoengsum: {score}");
+                return;
+            }
+
+            var correct = (a < b && input == "<")
+                       || (a > b && input == ">")
+                       || (a == b && input == "=");
+
+            if (correct)
+            {
+                score++;
+                Console.WriteLine($"Riktig! Poeng: {score}");
+            }
+            else
+            {
+                score--;
+                Console.WriteLine($"Feil. Poeng: {score}");
+            }
+            Console.WriteLine();
+        }
     }
 }
