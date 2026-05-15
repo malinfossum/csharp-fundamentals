537b98bb19809405590b9bd542d34e31c27f5711
Malin Fossum <malinfossum.dev@proton.me>
2026-05-15 16:42:31 +0200

    feat: implement GuessTheNumber

--- GuessTheNumber.cs
+++ GuessTheNumber.cs
@@ -1,10 +1,41 @@
 namespace CsharpFundamentals;
 
-// Curriculum: Gjett tallet
+// Modul 2: Gjett Tallet!
 public class GuessTheNumber
 {
     public void Run()
     {
-        Console.WriteLine("Not yet implemented");
+        while (true)
+        {
+            var target = Random.Shared.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write("Gjett et tall mellom 1 og 100: ");
+                if (!int.TryParse(Console.ReadLine(), out int guess))
+                {
+                    Console.WriteLine("Ikke et tall.");
+                    continue;
+                }
+
+                if (guess < target)
+                {
+                    Console.WriteLine("Høyere!");
+                }
+                else if (guess > target)
+                {
+                    Console.WriteLine("Lavere!");
+                }
+                else
+                {
+                    Console.WriteLine($"Riktig! Tallet var {target}.");
+                    break;
+                }
+            }
+
+            Console.Write("Spille på nytt? (j/n): ");
+            if (Console.ReadLine() != "j") return;
+            Console.WriteLine();
+        }
     }
 }
