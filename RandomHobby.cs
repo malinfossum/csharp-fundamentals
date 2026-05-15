7935a710c54978460330fcee6ecdf6f54bb45500
Malin Fossum <malinfossum.dev@proton.me>
2026-05-15 16:40:09 +0200

    feat: implement RandomHobby

--- RandomHobby.cs
+++ RandomHobby.cs
@@ -1,10 +1,25 @@
 namespace CsharpFundamentals;
 
-// Curriculum: Random Hobby
+// Modul 2: Random Hobby
 public class RandomHobby
 {
     public void Run()
     {
-        Console.WriteLine("Not yet implemented");
+        Console.Write("Who would like a new hobby? ");
+        var name = Console.ReadLine();
+
+        var hobbies = new[]
+        {
+            "magician. Poof, magic!",
+            "official toad-licker. Sorry.",
+            "competitive cheese-roller. Watch out below!",
+            "amateur cloud-namer. Look, a sheep!",
+            "professional puddle-jumper. Splash!"
+        };
+
+        var index = Random.Shared.Next(hobbies.Length);
+        var hobby = hobbies[index];
+
+        Console.WriteLine($"{name} is now a {hobby}");
     }
 }
